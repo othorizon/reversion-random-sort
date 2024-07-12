8fae9b7c372bd0356a107a19c40f2908c579d31d
rizon <othorizon@users.noreply.github.com>
2024-07-13 01:03:16 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import RunningComp from "./components/running";
 import StartComp from "./components/start";
 import ResultComp from "./components/result";
+import { Button } from "@/components/ui/button";
+import { PlayIcon, Volume2Icon, VolumeXIcon } from "lucide-react";
 
 
 
@@ -17,10 +19,11 @@ async function sleep(duration: number) {
   });
 }
 
-
+const audio = new Audio("/ShadowsAndDust-chosic.com_.mp3");
 export default function Home() {
   const revData = useRevData();
   const speedRef = useRef(revData.speed);
+  const [play, setPlay] = useState(true);
 
   const runWorker = async () => {
     while (true) {
@@ -42,10 +45,31 @@ export default function Home() {
 
   }, [revData.startTime]);
 
+  useEffect(() => {
+    playSound();
+  }, []);
+
+  const playSound = () => {
+    audio.play();
+    setPlay(true);
+  }
+  const stopSound = () => {
+    audio.pause();
+    setPlay(false);
+  }
+
 
 
   return (
     <main className="w-screen h-screen flex  flex-col gap-20 items-center justify-start p-24">
+      <Button size="icon" className="opacity-15 hover:opacity-100 absolute right-4 top-4 rounded-full h-8 w-8" onClick={play ? stopSound : playSound}>
+        {play ? (
+          <Volume2Icon size="14" />
+        ) : (
+          <VolumeXIcon size="14" />
+        )}
+
+      </Button>
       <div className="w-full select-none text-4xl md:text-4xl lg:text-6xl flex justify-between font-extralight">
         <div className="md:mr-12">{"["}</div>
         {
